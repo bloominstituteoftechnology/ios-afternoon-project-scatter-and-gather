ffc33b17c02a200dc161944e03378ff9543ec6c9
Christopher Aronson <christopheraronson@icloud.com>
2019-05-15 12:29:39 -0700

    Init Commit

---
+++ BasicAnimatonsAfternoon/BasicAnimatonsAfternoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicAnimatonsAfternoon
+//
+//  Created by Christopher Aronson on 5/15/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
