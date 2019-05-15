e3495753eeef91a785cd5b52fe1399995baf97c8
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-15 17:23:56 -0400

    Set up color changes and fade

--- Scatter+Gather/Scatter+Gather/View Controller/ViewController.swift
+++ Scatter+Gather/Scatter+Gather/View Controller/ViewController.swift
@@ -26,11 +26,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        labels = [lambdaL, lambdaA, lambdaM, lambdaB, lambdaD, lambdA]
         
+        view.addSubview(lambdaL)
+        view.addSubview(lambdaA)
+        view.addSubview(lambdaM)
+        view.addSubview(lambdaB)
+        view.addSubview(lambdaD)
+        view.addSubview(lambdA)
+        view.addSubview(logo)
     }
 
     @IBAction func toggle(_ sender: Any) {
         
+        if shouldScramble == false {
+            UIView.animate(withDuration: 3.0) {
+                self.logo.alpha = 0.0
+                
+                for label in self.labels {
+                    let index = Int(arc4random_uniform(UInt32(self.colorArray.count)))
+                    label.textColor = self.colorArray[index]
+                    let backgroundIndex = Int(arc4random_uniform(UInt32(self.colorArray.count)))
+                    label.layer.backgroundColor = self.colorArray[backgroundIndex].cgColor
+                }
+            }
+            
+            shouldScramble = true
+        } else {
+            UIView.animate(withDuration: 3.0) {
+                self.logo.alpha = 1.0
+                
+                for label in self.labels {
+                    label.textColor = .black
+                    label.layer.backgroundColor = UIColor.clear.cgColor
+                }
+            }
+            
+            shouldScramble = false
+        }
     }
     
 }
