de10d682f9907532ff3aa4a46e2db4c37e42d6ff
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-15 17:08:56 -0400

    Set up outlets

--- Scatter+Gather/Scatter+Gather/View Controller/ViewController.swift
+++ Scatter+Gather/Scatter+Gather/View Controller/ViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: - Properties
+    // MARK: - Properties and Outlets
     var shouldScramble: Bool = false
-
+    let colorArray = [UIColor.red, UIColor.orange, UIColor.blue, UIColor.cyan, UIColor.purple, UIColor.green, UIColor.magenta, UIColor.yellow]
+    private var labels: [UILabel] = []
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var lambdaL: UILabel!
+    @IBOutlet weak var lambdaA: UILabel!
+    @IBOutlet weak var lambdaM: UILabel!
+    @IBOutlet weak var lambdaB: UILabel!
+    @IBOutlet weak var lambdaD: UILabel!
+    @IBOutlet weak var lambdA: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
     @IBAction func toggle(_ sender: Any) {
