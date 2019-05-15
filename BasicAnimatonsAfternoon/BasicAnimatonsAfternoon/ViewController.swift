1e00bbd540834c5afcf91ece5b7edccd2bcc07cc
Christopher Aronson <christopheraronson@icloud.com>
2019-05-15 14:36:13 -0700

    Finished app

--- BasicAnimatonsAfternoon/BasicAnimatonsAfternoon/ViewController.swift
+++ BasicAnimatonsAfternoon/BasicAnimatonsAfternoon/ViewController.swift
@@ -32,32 +32,84 @@ class ViewController: UIViewController {
         if shouldScramble {
             fadeLogo()
             scramble()
+            changeBackground()
             shouldScramble = !shouldScramble
         } else {
             fadeInLogo()
             reset()
+            changeBackgroundBack()
             shouldScramble = !shouldScramble
         }
     }
     
+    // MARK: - Background Color Functions
+    func changeBackground() {
+        UIView.animate(withDuration: 3) {
+            self.lLabel.layer.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1).cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.aLabel.layer.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1).cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.mLabel.layer.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1).cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.bLabel.layer.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1).cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.dLabel.layer.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1).cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.finalALabel.layer.backgroundColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1).cgColor
+        }
+    }
+    
+    func changeBackgroundBack() {
+        UIView.animate(withDuration: 3) {
+            self.lLabel.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.aLabel.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.mLabel.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.bLabel.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.dLabel.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        UIView.animate(withDuration: 3) {
+            self.finalALabel.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    // MARK: - Translation Functions
     func scramble() {
         UIView.animate(withDuration: 3) {
             self.lLabel.transform = CGAffineTransform(translationX: CGFloat.random(in: -100...300), y: CGFloat.random(in: -100...700)).rotated(by: CGFloat.pi / CGFloat.random(in: 0...16))
+            self.lLabel.textColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1)
         }
         UIView.animate(withDuration: 3) {
             self.aLabel.transform = CGAffineTransform(translationX: CGFloat.random(in: -150...250), y: CGFloat.random(in: -100...700)).rotated(by: CGFloat.pi / CGFloat.random(in: 0...16))
+            self.aLabel.textColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1)
         }
         UIView.animate(withDuration: 3) {
             self.mLabel.transform = CGAffineTransform(translationX: CGFloat.random(in: -200...200), y: CGFloat.random(in: -100...700)).rotated(by: CGFloat.pi / CGFloat.random(in: 0...16))
+            self.mLabel.textColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1)
         }
         UIView.animate(withDuration: 3) {
             self.bLabel.transform = CGAffineTransform(translationX: CGFloat.random(in: -250...150), y: CGFloat.random(in: -100...700)).rotated(by: CGFloat.pi / CGFloat.random(in: 0...16))
+            self.bLabel.textColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1)
         }
         UIView.animate(withDuration: 3) {
             self.dLabel.transform = CGAffineTransform(translationX: CGFloat.random(in: -300...100), y: CGFloat.random(in: -100...700)).rotated(by: CGFloat.pi / CGFloat.random(in: 0...16))
+            self.dLabel.textColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1)
         }
         UIView.animate(withDuration: 3) {
             self.finalALabel.transform = CGAffineTransform(translationX: CGFloat.random(in: -350...50), y: CGFloat.random(in: -100...700)).rotated(by: CGFloat.pi / CGFloat.random(in: 0...16))
+            self.finalALabel.textColor = UIColor(hue: CGFloat.random(in: 0..<1), saturation: CGFloat.random(in: 0..<1), brightness: CGFloat.random(in: 0..<1), alpha: 1)
         }
         
     }
@@ -65,21 +117,27 @@ class ViewController: UIViewController {
     func reset() {
         UIView.animate(withDuration: 3) {
             self.lLabel.transform = .identity
+            self.lLabel.textColor = .black
         }
         UIView.animate(withDuration: 3) {
             self.aLabel.transform = .identity
+            self.aLabel.textColor = .black
         }
         UIView.animate(withDuration: 3) {
             self.mLabel.transform = .identity
+            self.mLabel.textColor = .black
         }
         UIView.animate(withDuration: 3) {
             self.bLabel.transform = .identity
+            self.bLabel.textColor = .black
         }
         UIView.animate(withDuration: 3) {
             self.dLabel.transform = .identity
+            self.dLabel.textColor = .black
         }
         UIView.animate(withDuration: 3) {
             self.finalALabel.transform = .identity
+            self.finalALabel.textColor = .black
         }
     }
     
