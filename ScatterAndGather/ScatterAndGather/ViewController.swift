5ae2d1df85ce5c6088b44845a00a2beadcac7bc3
jayfenam <jonathanferrer95@gmail.com>
2019-05-22 15:48:55 -0400

    created the lambdaLogo fade out and in functions

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lambdaLogo: UIImageView!
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
@@ -26,6 +27,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func toggleButtonPressed(_ sender: Any) {
+        if isScattered == false {
+            fadeOutLogo()
+            isScattered = true
+        } else {
+            fadeInLogo()
+        }
+    }
+
+    func fadeOutLogo() {
+        UIView.animate(withDuration: 2) {
+           self.lambdaLogo.alpha = 0
+        }
+    }
+    func fadeInLogo() {
+        UIView.animate(withDuration: 2) {
+            self.lambdaLogo.alpha = 1
+        }
     }
 
 }
