e85c9232c6aa018948c553c683e770bbff0de215
jayfenam <jonathanferrer95@gmail.com>
2019-05-22 15:37:54 -0400

    created the project,  added the UI labels and the Lambda Image

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Jonathan Ferrer on 5/22/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    @IBOutlet weak var label6: UILabel!
+    
+    var isScattered: Bool = false
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+
+}
+
