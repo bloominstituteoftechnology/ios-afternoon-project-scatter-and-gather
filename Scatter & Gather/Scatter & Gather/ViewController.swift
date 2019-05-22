0a2dea11e196910818e63054200f61cce9b3e224
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-22 15:38:26 -0600

    Working app for MVP

--- Scatter & Gather/Scatter & Gather/ViewController.swift
+++ Scatter & Gather/Scatter & Gather/ViewController.swift
@@ -12,13 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(
+        lLabel.center,
+        firstaLabel.center,
+        mLabel.center,
+        bLabel.center,
+        dLabel.center,
+        secondaLabel.center)
         logoImageView.image = lambdaLogoImage
+        labels += [lLabel, firstaLabel, mLabel, bLabel, dLabel, secondaLabel]
     }
     
     var isScattered: Bool = true
+    let lOriginal = CGPoint(x: 50.0, y: 24.0)
+    let a1Original = CGPoint(x: 100.0, y: 24.0)
+    let mOriginal = CGPoint(x: 150.0, y: 24.0)
+    let bOriginal = CGPoint(x: 200.0, y: 24.0)
+    let dOriginal = CGPoint(x: 250.0, y: 24.0)
+    let a2Original = CGPoint(x: 300.0, y: 24.0)
     
     @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
-        print(isScattered)
+        print(lLabel.center)
         if isScattered == true {
             scatterAnimation()
             isScattered = false
@@ -36,17 +50,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondaLabel: UILabel!
     @IBOutlet weak var logoImageView: UIImageView!
     var lambdaLogoImage = UIImage(named: "lambda_logo")
-    
+    var labels : [UILabel] = []
+    var colors : [UIColor] = [.red, .yellow, .blue, .orange, .green, .purple, .black, .brown]
+ 
     func scatterAnimation() {
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 4) {
             self.logoImageView.alpha = 0
+            for label in self.labels {
+                let randomX = Int.random(in: 50...300)
+                let randomY = Int.random(in: -50...600)
+                let randomPoint = CGPoint(x: randomX, y: randomY)
+                let randomAngle = CGFloat.random(in: 0.0...360.0)
+                label.center = randomPoint
+                label.transform = CGAffineTransform(rotationAngle: randomAngle)
+                label.backgroundColor = self.colors.randomElement()
+                label.textColor = self.colors.randomElement()
+            }
         }
     }
     
     func gatherAnimation() {
         logoImageView.alpha = 0
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 4) {
             self.logoImageView.alpha = 1
+            for label in self.labels {
+                label.transform = .identity
+                label.backgroundColor = .white
+                label.textColor = .black
+            }
+            self.lLabel.center = self.lOriginal
+            self.firstaLabel.center = self.a1Original
+            self.mLabel.center = self.mOriginal
+            self.bLabel.center = self.bOriginal
+            self.dLabel.center = self.dOriginal
+            self.secondaLabel.center = self.a2Original
         }
     }
 }
