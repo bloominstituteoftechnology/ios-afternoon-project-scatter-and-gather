4a05ce47b56f834b5248d7f795fdd5d693192c54
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-06-11 21:13:12 -0400

    Added a Bar Button to the Navigation Bar

--- Scatter and Gather/Scatter and Gather/View Controllers/ViewController.swift
+++ Scatter and Gather/Scatter and Gather/View Controllers/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    var isScattered: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - IBActions and Methods
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        isScattered = !isScattered
+    }
+    
 }
 
