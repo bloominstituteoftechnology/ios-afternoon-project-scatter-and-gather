97638e541d872a436331de31d987506f5382ec4f
Kat Milton <mltnkat@gmail.com>
2019-07-17 12:08:13 -0700

    Initial commit.

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Kat Milton on 7/17/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
