9be80bafcbc3128cbc7b657b68806f553b1943b1
Alex Shillingford <alex.shillingford@icloud.com>
2019-08-14 22:30:48 -0600

    MVP achieved

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -20,72 +20,105 @@ class ViewController: UIViewController {
     
     
     var isScattered: Bool = false
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        if isScattered {
+            gather()
+            labelReturn()
+            isScattered = false
+        } else {
+            scatter()
+            labelRandomizer()
+            isScattered = true
+        }
+    }
+    
+    func gather() {
+        UIView.animate(withDuration: 1, animations: {
+            self.lambdaImageView.alpha = 1
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.82) {
+            self.firstLabel.center.x -= 100
+            self.firstLabel.center.y -= 57
+            self.aLabel.center.x -= 300
+            self.aLabel.center.y -= 200
+            self.mLabel.center.x -= 65
+            self.mLabel.center.y -= 20
+            self.bLabel.center.x -= 20
+            self.bLabel.center.y -= 475
+            self.dLabel.center.x -= 20
+            self.dLabel.center.y -= 157
+            self.secondALabel.center.x += 200
+            self.secondALabel.center.y -= 500
+        }
+    }
+    
+    func scatter() {
         UIView.animate(withDuration: 1, animations: {
             self.lambdaImageView.alpha = 0
         }, completion: nil)
-        if isScattered {
-            /*
-            let gather = {
-                UIView.addKeyframe(withRelativeStartTime: 1, relativeDuration: 1, animations: {
-                    self.firstLabel.center.x -= 100
-                    self.firstLabel.center.y -= 57
-                    self.aLabel.center.x -= 300
-                    self.aLabel.center.y -= 200
-                    self.mLabel.center.x -= 65
-                    self.mLabel.center.y -= 20
-                    self.bLabel.center.x -= 20
-                    self.bLabel.center.y -= 475
-                    self.dLabel.center.x -= 20
-                    self.dLabel.center.y -= 157
-                    self.secondALabel.center.x += 200
-                })
-            }
-            UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: gather, completion: nil)
-            */
-            UIView.animate(withDuration: 0.82) {
-                self.firstLabel.center.x -= 100
-                self.firstLabel.center.y -= 57
-                self.aLabel.center.x -= 300
-                self.aLabel.center.y -= 200
-                self.mLabel.center.x -= 65
-                self.mLabel.center.y -= 20
-                self.bLabel.center.x -= 20
-                self.bLabel.center.y -= 475
-                self.dLabel.center.x -= 20
-                self.dLabel.center.y -= 157
-                self.secondALabel.center.x += 200
-                self.secondALabel.center.y += 500
-            }
-            isScattered = false
-        } else {
-            let scatter = {
-                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1, animations: {
-                    self.firstLabel.center.x += 100
-                    self.firstLabel.center.y += 57
-                    self.aLabel.center.x += 300
-                    self.aLabel.center.y += 200
-                    self.mLabel.center.x += 65
-                    self.mLabel.center.y += 20
-                    self.bLabel.center.x += 20
-                    self.bLabel.center.y += 475
-                    self.dLabel.center.x += 20
-                    self.dLabel.center.y += 157
-                    self.secondALabel.center.x -= 200
-                    self.secondALabel.center.y += 500
-                })
-            }
-            UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: scatter, completion: nil)
-            isScattered = true
+        
+        UIView.animate(withDuration: 0.82) {
+            self.firstLabel.center.x += 100
+            self.firstLabel.center.y += 57
+            self.aLabel.center.x += 300
+            self.aLabel.center.y += 200
+            self.mLabel.center.x += 65
+            self.mLabel.center.y += 20
+            self.bLabel.center.x += 20
+            self.bLabel.center.y += 475
+            self.dLabel.center.x += 20
+            self.dLabel.center.y += 157
+            self.secondALabel.center.x -= 200
+            self.secondALabel.center.y += 500
         }
-        
-        
+    }
+    
+    func labelRandomizer() {
+        UIView.animateKeyframes(withDuration: 0.82, delay: 0, options: [], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.82, animations: {
+                self.firstLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 4)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.82, animations: {
+                self.aLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.82, animations: {
+                self.mLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 3)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.82, animations: {
+                self.bLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 1)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.82, animations: {
+                self.dLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 0.5)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.82, animations: {
+                self.secondALabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 7)
+            })
+        })
+    }
+    
+    func labelReturn() {
+        UIView.animate(withDuration: 0.82) {
+            self.firstLabel.transform = .identity
+            self.aLabel.transform = .identity
+            self.mLabel.transform = .identity
+            self.bLabel.transform = .identity
+            self.dLabel.transform = .identity
+            self.secondALabel.transform = .identity
+        }
     }
 }
+
 
