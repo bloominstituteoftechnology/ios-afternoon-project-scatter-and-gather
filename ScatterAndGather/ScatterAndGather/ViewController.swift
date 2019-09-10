4140d41901420387e07911ffd87785acfc785727
ladybeitel <ciarabeitel@gmail.com>
2019-09-10 16:24:10 -0400

    Set up project. Add toggle bar button item. Create toggle action and isScattered variable.

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Ciara Beitel on 9/10/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var isScattered: Bool = true
+    
+    @IBOutlet weak var toggleButtonPressed: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
