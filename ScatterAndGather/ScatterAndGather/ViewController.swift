2e413379eef8620aec0124ae00ab3a9761af5379
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-10 13:58:04 -0700

    Added nav

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Taylor Lyles on 9/10/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
