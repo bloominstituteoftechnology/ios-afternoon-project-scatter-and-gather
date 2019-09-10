0ab91125941d2f8f4d3684769e3876fc87054048
Andrew Ruiz <nukedoom@gmail.com>
2019-09-10 14:10:42 -0700

    Completed some labels.

--- Scatter and Gather/ViewController.swift
+++ Scatter and Gather/ViewController.swift
@@ -11,12 +11,48 @@ import UIKit
 // Testing Github
 
 class ViewController: UIViewController {
+    
+    let isScattered : Bool = false
+    
+    private var letter1: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpSubViews()
+    }
+    
+    
+    private func setUpSubViews() {
+        
+        let letter1 = UILabel()
+        letter1.translatesAutoresizingMaskIntoConstraints = false
+        letter1.text = "Hello"
+        view.addSubview(letter1)
+        
+        self.letter1 = letter1
+        
+        
+        let letter2 = UILabel()
+        letter2.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(letter2)
+        
+        let letter3 = UILabel()
+        letter3.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(letter3)
+        
+        let letter4 = UILabel()
+        letter4.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(letter4)
+        
+        let letter5 = UILabel()
+        letter5.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(letter5)
     }
 
 
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+        
+        
+    }
 }
 
