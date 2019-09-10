5b7a9e6bae20b06cdf581e98d102e5b1d382e92a
Percy <percival.ngan@gmail.com>
2019-09-10 16:42:02 -0500

    Added labels and imageView to view.

---
+++ ScatterGatherAnimations/ScatterGatherAnimations/View Controllers/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ScatterGatherAnimations
+//
+//  Created by Percy Ngan on 9/10/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	// MARK: - Outlets
+
+	@IBOutlet weak var lLabel: UILabel!
+	@IBOutlet weak var aLabel: UILabel!
+	@IBOutlet weak var mLabel: UILabel!
+	@IBOutlet weak var bLabel: UILabel!
+	@IBOutlet weak var dLabel: UILabel!
+	@IBOutlet weak var aLabel: UILabel!
+	@IBOutlet weak var logoImage: UIImageView!
+	@IBOutlet weak var toggleButtonPressed: UIBarButtonItem!
+
+
+	// MARK: - Properties
+
+	var isScattered: Bool = true
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	@IBOutlet weak var toogleButtonPressed: UIImageView!
+	
+}
+
