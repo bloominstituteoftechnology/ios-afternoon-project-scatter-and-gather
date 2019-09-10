7a12aef9255e41d3e449a15176ee3b0a2db36a53
Jerry Haaser <jerry.haaser@hotmail.com>
2019-09-10 15:27:08 -0500

    created initial View

---
+++ LambdaScatter/LambdaScatter/LambdaScatterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LambdaScatterViewController.swift
+//  LambdaScatter
+//
+//  Created by admin on 9/10/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class LambdaScatterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
