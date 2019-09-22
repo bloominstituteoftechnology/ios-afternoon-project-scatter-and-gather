94a0db8c95be4ff114f5efd9949912936bf05eb0
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-21 20:41:49 -0400

    set up the project

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Vici Shaweddy on 9/21/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var isScattered: Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+    
+}
+
