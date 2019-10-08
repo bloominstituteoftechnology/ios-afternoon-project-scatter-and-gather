ad8b11923d998aafb62c207d7333c2e140013edd
Isvvc <lyons@tuta.io>
2019-10-08 15:17:31 -0600

    Add image with fade

--- Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -11,14 +11,79 @@ import UIKit
 class ViewController: UIViewController {
     
     var isScattered = false
+    let labelText: String = "Lambda"
+    var labels: [UILabel] = []
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        for label in labels {
+            label.center = self.view.center
+        }
+    }
+    
+    private func configureViews() {
+        let imageView = UIImageView(image: UIImage(named: "lambda_logo"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        //imageView.layer.borderWidth = 2
+        //imageView.layer.cornerRadius = 12
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+        
+        self.imageView = imageView
+        
+        labels = []
+        
+        //let totalLetters = labelText.count
+        for letter in labelText {
+            print(letter)
+            let label = UILabel()
+            label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
+            label.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(label)
+            
+            label.layer.borderWidth = 2
+            label.layer.cornerRadius = 12
+            
+            label.text = "\(letter)"
+            label.textAlignment = .center
+            label.font = UIFont.monospacedSystemFont(ofSize: 48, weight: .medium)
+            
+            labels.append(label)
+        }
+    }
+    
+    private func scatter() {
+        UIView.animate(withDuration: 2) {
+            self.imageView.alpha = 0
+        }
+    }
+    
+    private func gather() {
+        UIView.animate(withDuration: 2) {
+            self.imageView.alpha = 100
+        }
     }
 
     @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
         isScattered.toggle()
+        if isScattered {
+            scatter()
+        } else {
+            gather()
+        }
     }
     
 }
