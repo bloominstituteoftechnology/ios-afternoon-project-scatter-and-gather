377d23d1c18e250a2c51eefdb4b142dfc64c4ba9
Percy <percival.ngan@gmail.com>
2019-10-08 10:44:11 -0500

    Initial commit, setup view and VC, added Toggle button

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Percy Ngan on 10/8/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	var isScattered: Bool = false
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	// MARK: - Actions
+
+	@IBAction func toggleuttonPressed(_ sender: Any) {
+	}
+}
+
