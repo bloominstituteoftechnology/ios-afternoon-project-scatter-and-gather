5212a1cbde35f9ba646dbc45cd397c89daba5ac6
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-10-08 15:24:33 -0700

    MVP

--- BasicAnimation-Scatter-and-Gather/BasicAnimation-Scatter-and-Gather/ViewController.swift
+++ BasicAnimation-Scatter-and-Gather/BasicAnimation-Scatter-and-Gather/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 // MARK: Properties
     
     var isScattered = false
+    var labels: [UILabel] = []
     
 // MARK: Outlets
     
@@ -29,14 +30,42 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 // MARK: Actions
     
     // isScattered = boolean that can be toggled
     @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
-        isScattered.toggle()
+            if isScattered {
+                gatherAnimation()
+                UIView.animate(withDuration: 4, animations: {
+                    self.lambdaLogoImageView.alpha = 1
+                }, completion: nil)
+            } else {
+                scatterAnimation()
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.lambdaLogoImageView.alpha = 0
+                }, completion: nil)
+            }
+            isScattered = !isScattered
+        }
+    
+        func gatherAnimation() {
+        let labels: [UILabel] = [lLabel, firstALabel, mLabel, bLabel, dLabel, secondALabel]
+        
+        for i in labels {
+            reset(label: i)
+        }
     }
+    
+    func scatterAnimation() {
+        let labels: [UILabel] = [lLabel, firstALabel, mLabel, bLabel, dLabel, secondALabel]
+        for i in labels {
+            animate(label: i)
+        }
+    }
+    
     func fadeLogo() {
         UIView.animateKeyframes(withDuration: 4, delay: 0,options: [], animations: {
            
@@ -51,8 +80,47 @@ class ViewController: UIViewController {
             }
         },
         completion: nil)
-    
-    
 }
+    
+    func animate(label: UILabel) {
+            
+            //Background Color
+            UILabel.animate(withDuration: 4, animations: {
+                label.layer.backgroundColor = UIColor.random().cgColor
+            }, completion: nil)
+        
+            //Rotation And Position
+            UILabel.animate(withDuration: 4) {
+                label.transform = CGAffineTransform(rotationAngle: .random(in: 0...10))
+                    .concatenating(CGAffineTransform(translationX: CGFloat.random(in: -250...300), y: CGFloat.random(in: -300...400)))
+            }
+            
+            //Text Color
+            UILabel.animate(withDuration: 4, animations: {
+                label.textColor = UIColor.random()
+            }, completion: nil)
+        }
+    }
+        func reset(label: UILabel) {
+            UIView.animate(withDuration: 4, animations: {
+                label.transform = .identity
+                label.layer.backgroundColor = UIColor.clear.cgColor
+                label.textColor = UIColor.yellow
+            }, completion: nil)
+        }
+            
+            extension UIColor {
 
-}
+                static func random() -> UIColor {
+                    let hue = CGFloat.random(in: 0...1)
+                    let saturation = CGFloat.random(in: 0.2...1)
+                    let brightness = CGFloat.random(in: 0.2...1)
+                    let alpha = CGFloat.random(in: 0.5...1)
+                    
+                    let color = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+                    
+                    return color
+                }
+        }
+
+
