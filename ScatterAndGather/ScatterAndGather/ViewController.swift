5c98afb0e55ca5ddb7bc0ce08a6d9e2a70aa454f
Brandi <blubrandi0@gmail.com>
2019-11-05 13:31:08 -0700

    Added Xcode project

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by BDawg on 11/5/19.
+//  Copyright © 2019 Brandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
