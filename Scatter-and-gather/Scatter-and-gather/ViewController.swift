87845a26d9cf472de17f9d99d80073eb873e50f0
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-11-05 14:39:50 -0800

    Added functionality for all labels to move when toggled back and forth back to origins

--- Scatter-and-gather/Scatter-and-gather/ViewController.swift
+++ Scatter-and-gather/Scatter-and-gather/ViewController.swift
@@ -26,11 +26,30 @@ class ViewController: UIViewController {
     // MARK: - Properties
     
     var isScattered = false
+
+    var charLOriginPoint: CGPoint!
+    var charAOriginPoint: CGPoint!
+    var charMOriginPoint: CGPoint!
+    var charBOriginPoint: CGPoint!
+    var charDOriginPoint: CGPoint!
+    var charAAOriginPoint: CGPoint!
+        
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setLabelPointOrigins()
         logoImageView.isOpaque = false
+    }
+    
+    private func setLabelPointOrigins() {
+        charLOriginPoint = self.charLLabel.center
+        charAOriginPoint = self.charALabel.center
+        charMOriginPoint = self.charMLabel.center
+        charBOriginPoint = self.charBLabel.center
+        charDOriginPoint = self.charDLabel.center
+        charAAOriginPoint = self.charAALabel.center
     }
 
     // MARK: - IBActions
@@ -48,28 +67,47 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     private func scatter() {
-
-        let minX = Int(self.view.bounds.minX)
-        let maxX = Int(self.view.bounds.maxX)
-        let minY = Int(self.view.bounds.minY)
-        let maxY = Int(self.view.bounds.maxY)
-        
-        
-        let randXPoint = Int.random(in: minX...maxX)
-        let randYPoint = Int.random(in: minY...maxY)
-        
-        
         
         UIView.animate(withDuration: 2.0) {
-            self.charLLabel.center = CGPoint(x: CGFloat(randXPoint), y: CGFloat(randYPoint))
+            self.charLLabel.center = self.randomPoint(from: self.charLLabel)
+            self.charALabel.center = self.randomPoint(from: self.charALabel)
+            self.charMLabel.center = self.randomPoint(from: self.charMLabel)
+            self.charBLabel.center = self.randomPoint(from: self.charBLabel)
+            self.charDLabel.center = self.randomPoint(from: self.charDLabel)
+            self.charAALabel.center = self.randomPoint(from: self.charAALabel)
             self.logoImageView.alpha = 0.0
         }
         
     }
     
+    private func randomPoint(from label: UILabel) -> CGPoint {
+        
+        let minX = Int(view.bounds.minX)
+        var maxX = Int(view.bounds.maxX)
+        let minY = Int(view.bounds.minY)
+        var maxY = Int(view.bounds.maxY)
+        
+        let labelHeight = Int(label.bounds.size.height)
+        let labelWidth = Int(label.bounds.size.width)
+        
+        maxX = maxX - labelWidth
+        maxY = maxY - labelHeight
+        
+        let randXPoint = CGFloat(Int.random(in: minX...maxX))
+        let randYPoint = CGFloat(Int.random(in: minY...maxY))
+        return CGPoint(x: randXPoint, y: randYPoint)
+    }
+    
+    
     private func gather() {
         
         UIView.animate(withDuration: 2.0) {
+            self.charLLabel.center = self.charLOriginPoint
+            self.charALabel.center = self.charAOriginPoint
+            self.charMLabel.center = self.charMOriginPoint
+            self.charBLabel.center = self.charBOriginPoint
+            self.charDLabel.center = self.charDOriginPoint
+            self.charAALabel.center = self.charAAOriginPoint
             self.logoImageView.alpha = 1.0
         }
     }
