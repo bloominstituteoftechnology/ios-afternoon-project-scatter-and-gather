f854236d033dfc77923c3190153335be8b3cfdef
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-11-05 12:07:09 -0800

    Added project Scatter-and-gather

---
+++ Scatter-and-gather/Scatter-and-gather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Scatter-and-gather
+//
+//  Created by Lambda_School_Loaner_204 on 11/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+    var isScattered = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - IBActions
+    @IBAction func toggleButtonPressed(sender: UIBarButtonItem) {
+        
+    }
+}
+
