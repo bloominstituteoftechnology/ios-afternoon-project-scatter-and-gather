103b51b0cf28da64f7e0aec1b8f884f916b9c03d
Craig Swanson <cswanson02@me.com>
2019-11-14 16:07:39 -0600

    Created file, viewcontroller, and Toggle button

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Craig Swanson on 11/14/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Properties
+    var isScattered: Bool = false
+    
+    
+    // MARK: Outlets
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    @IBAction func togglebuttonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+
+
+}
+
