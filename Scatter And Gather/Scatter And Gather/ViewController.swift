a0e0526e6c5d8eef08c7e447060554bbb297362a
Joseph Rogers <joerogersss2016@gmail.com>
2019-11-15 00:42:09 -0600

    Base Project Skeleton
    Empty Xcode Project added

---
+++ Scatter And Gather/Scatter And Gather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter And Gather
+//
+//  Created by Joseph Rogers on 11/15/19.
+//  Copyright © 2019 Moka Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
