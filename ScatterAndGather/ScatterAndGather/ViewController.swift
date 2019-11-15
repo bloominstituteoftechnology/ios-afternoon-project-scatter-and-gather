a86540e216bd2d2eeb86a30a4bd866332201f5eb
Craig Swanson <cswanson02@me.com>
2019-11-14 22:09:53 -0600

    Updated offsets of animation ending points based on device screen size.

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     var isScattered: Bool = false
-    let animationDuration = 3.5
+    var animationDuration: Double = 0
+    var screenWidth: CGFloat = 0
+    var screenHeight: CGFloat = 0
     var letterStartPoint: [CGPoint] = []
     var letterList: [UILabel] = []
     
@@ -45,6 +47,13 @@ class ViewController: UIViewController {
             isScattered = false
         } else {
             
+            // set screenWidth and screenHeight based on the device screen size
+            screenWidth = view.bounds.width
+            screenHeight = view.bounds.height
+            
+            // set the duration to a random double bewteen 2 - 4 seconds
+            animationDuration = Double.random(in: 2.0...4.0)
+            
             // if isScatter is false, then the scatterAnimation is run.
             // Set the array of the starting points of each letter so we know where to return.
             letterStartPoint = [letterL.center, letterA.center, letterM.center, letterB.center, letterD.center, letterA2.center]
@@ -66,7 +75,7 @@ class ViewController: UIViewController {
             }
             UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0) {
                 for letter in self.letterList {
-                    letter.center = CGPoint(x: self.view.center.x + CGFloat.random(in: -250...150), y: self.view.center.y + CGFloat.random(in: -450...200))
+                    letter.center = CGPoint(x: self.view.center.x + CGFloat.random(in: -(self.screenWidth / 1.75)...self.screenWidth / 3), y: self.view.center.y + CGFloat.random(in: -(self.screenHeight / 2)...self.screenHeight / 3))
                 }
             }
         }
