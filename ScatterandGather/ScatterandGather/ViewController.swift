230951c32343fc5d48f9d46e2471e714f8be7eff
Aaron Haughton <dreamcast2000@outlook.com>
2019-11-17 20:21:00 -0500

    Initial Commit. Set up Storyboard

---
+++ ScatterandGather/ScatterandGather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterandGather
+//
+//  Created by Joe Thunder on 11/17/19.
+//  Copyright © 2019 LambdaSchool.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
