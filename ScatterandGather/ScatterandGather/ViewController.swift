1170ef2a20e56b70faf151aa9992d273cf2b116f
Aaron Haughton <dreamcast2000@outlook.com>
2019-11-18 13:04:58 -0500

    ImageView fade in and out is working.

--- ScatterandGather/ScatterandGather/ViewController.swift
+++ ScatterandGather/ScatterandGather/ViewController.swift
@@ -27,14 +27,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func toggleButtonPressed(_ sender: Any) {
+        toggleLogo()
+//        toggleLetters()
+    }
+    
+    // MARK: - toggles logo image to fade in and out
+    func toggleLogo() {
+        isScattered.toggle()
+        if isScattered == true {
+            UIView.animate(withDuration: 1.5, delay: 0.0, options: .curveEaseOut, animations: {
+                self.logoImageView.alpha = 0.0
+            }, completion: nil)
+            
+             } else if isScattered == false {
+            UIView.animate(withDuration: 1.5, delay: 0.0, options: .curveEaseIn, animations: {
+                self.logoImageView.alpha = 1.0
+            }, completion: nil)
+        }
+    }
+    
+    // MARK: - toggles Lambda letters to rotate and move.
+    func toggleLetters() {
+        if isScattered == true {
+                   
+            } else if isScattered == false {
+                   
+        }
     }
     
     
-    fadeOutLogo() {
-        
-        
-        
-    }
     
     
     
