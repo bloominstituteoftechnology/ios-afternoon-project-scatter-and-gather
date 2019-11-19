49bd369af2af9b9452eefa98a36166f55f2afe45
Aaron Haughton <dreamcast2000@outlook.com>
2019-11-18 19:53:14 -0500

    Added animations to letters. It's not pretty.

--- ScatterandGather/ScatterandGather/ViewController.swift
+++ ScatterandGather/ScatterandGather/ViewController.swift
@@ -20,20 +20,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var alphaTwoLabel: UILabel!
     
     var isScattered: Bool = false
+    var initalPoint: [CGPoint] = []
+    var number: Int = 0
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadInitalPoint()
     }
 
     @IBAction func toggleButtonPressed(_ sender: Any) {
+        isScattered.toggle()
         toggleLogo()
-//        toggleLetters()
+        toggleLetters()
     }
     
     // MARK: - toggles logo image to fade in and out
     func toggleLogo() {
-        isScattered.toggle()
+        
         if isScattered == true {
             UIView.animate(withDuration: 1.5, delay: 0.0, options: .curveEaseOut, animations: {
                 self.logoImageView.alpha = 0.0
@@ -49,15 +53,60 @@ class ViewController: UIViewController {
     // MARK: - toggles Lambda letters to rotate and move.
     func toggleLetters() {
         if isScattered == true {
-                   
+            randomMove(libraLabel)
+            randomMove(alphaOneLabel)
+            randomMove(mikeLabel)
+            randomMove(bravoLabel)
+            randomMove(deltaLabel)
+            randomMove(alphaTwoLabel)
             } else if isScattered == false {
-                   
+            putBack(libraLabel)
+            number += 1
+            putBack(alphaOneLabel)
+            number += 1
+            putBack(mikeLabel)
+            number += 1
+            putBack(bravoLabel)
+            number += 1
+            putBack(deltaLabel)
+            number += 1
+            putBack(alphaTwoLabel)
+            number = 0
         }
     }
     
     
+    func randomMove(_ letter: UILabel) {
+        let randomMove = {
+                       UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.5) {
+                           letter.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 0.0...360.0))
+                       }
+                       UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.5) {
+                           letter.center = CGPoint(x: CGFloat.random(in: 0.0...self.view.bounds.size.width), y: CGFloat.random(in: 0.0...self.view.bounds.size.height))
+                       }
+                   }
+                   UIView.animateKeyframes(withDuration: 1.5, delay: 0, options: [], animations: randomMove, completion: nil)
+    }
     
+    func putBack(_ letter: UILabel) {
+        let identityMove = {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.5) {
+                letter.transform = .identity
+                    letter.center = self.initalPoint[self.number]
+                }
+            }
+            UIView.animateKeyframes(withDuration: 1.5, delay: 0, options: [], animations: identityMove, completion: nil)
+        }
     
-    
+    func loadInitalPoint() {
+        initalPoint.append(libraLabel.center)
+        initalPoint.append(alphaOneLabel.center)
+        initalPoint.append(mikeLabel.center)
+        initalPoint.append(bravoLabel.center)
+        initalPoint.append(deltaLabel.center)
+        initalPoint.append(alphaTwoLabel.center)
+    }
 }
+
+
 
