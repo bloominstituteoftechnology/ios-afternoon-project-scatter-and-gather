4d9685f661c483b5b42e2f30233f8bc3040107d7
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-21 15:59:00 -0800

    logo and stack view done

--- ScatterGather/ScatterGather/ViewController.swift
+++ ScatterGather/ScatterGather/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    var lLabel: UILabel!
+    var aLabel: UILabel!
+    var mLabel: UILabel!
+    var bLabel: UILabel!
+    var dLabel: UILabel!
+    var lastALabel: UILabel!
+    
+    var logoImageView: UIImageView!
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLabels()
+        configureImage()
     }
 
+    func configureImage() {
+        // ImageView
+        logoImageView = UIImageView()
+        logoImageView.image = UIImage(named: "lambda_logo")
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoImageView)
+        
+        // Constraints
+        logoImageView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1).isActive = true
+        logoImageView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.9).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        logoImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    }
+    
+    func configureLabels() {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "LABEL"
+        label.backgroundColor = .red
+        view.addSubview(label)
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.text = "LABEL"
+        label2.backgroundColor = .blue
+        view.addSubview(label2)
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually// equalSpacing
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(label2)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+    }
+    
+    // MARK: - Actions
 
 }
 
