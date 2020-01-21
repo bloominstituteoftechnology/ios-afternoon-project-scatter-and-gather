34386b5bf07d4eb35c97b457a6763a8f54f6c193
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-21 12:56:32 -0800

    setup project

---
+++ ScatterGather/ScatterGather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterGather
+//
+//  Created by Jorge Alvarez on 1/21/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
