d7de440f728c591e21955e949460df182950f429
Aaron Cleveland <toscleveland@gmail.com>
2020-01-21 14:00:05 -0600

    Started Project & Updated Structure

---
+++ ScatterGatter/ScatterGatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterGatter
+//
+//  Created by Aaron Cleveland on 1/21/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
