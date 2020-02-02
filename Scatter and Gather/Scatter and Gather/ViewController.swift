b76804e2a4aee729fac2bc6a0ca71f2889fcf5af
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-02 18:03:52 -0500

    Initial Commit

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Joshua Rutkowski on 2/2/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
