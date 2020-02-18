6cdfa07714708c429d5b6459bbe7c93109f74b88
Enrique Gongora <EGTech24@gmail.com>
2020-02-18 12:47:58 -0800

    Initial Commit

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Enrique Gongora on 2/18/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        var isScattered: Bool
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
