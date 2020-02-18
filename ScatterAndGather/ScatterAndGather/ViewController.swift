f474e31954286f9d9efc3da4fae4236433269b23
Enrique Gongora <EGTech24@gmail.com>
2020-02-18 13:12:22 -0800

    Lambda Logo added with constraints

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -9,13 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Image
+    var imageView: UIImageView!
+    
+    //MARK: - IBAction
     @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
         var isScattered: Bool
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewSetUp()
+    }
+    
+    func viewSetUp() {
+        
+        //Image
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "lambda_logo")
+        view.addSubview(imageView)
+        self.imageView = imageView
+        
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, constant: 1.5).isActive = true
     }
 }
 
