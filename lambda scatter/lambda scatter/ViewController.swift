b5b48ed4eec2a21901d09f40581f1c7c170ee3c9
elizabeth paige <wingateep@Icloud.com>
2020-02-18 16:51:29 -0500

    set up the Main.storyboard

---
+++ lambda scatter/lambda scatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lambda scatter
+//
+//  Created by Elizabeth Wingate on 2/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
