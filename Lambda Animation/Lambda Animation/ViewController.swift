40ba99b66f5d624e33ee92158b50a2732ad02d33
Nick Nguyen <ptnguyen1901@gmail.com>
2020-02-18 22:09:10 -0500

    UIPanGestureRecognizer completed

--- Lambda Animation/Lambda Animation/ViewController.swift
+++ Lambda Animation/Lambda Animation/ViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIGestureRecognizerDelegate {
 
     
     // MARK: - Properties
     
     fileprivate(set) var isScattered = true
+    private var initialCenter = CGPoint()
     private var lambdaStackView : UIView!
-    private var lambdaImageView: UIImageView!
+    private var lambdaImageView: UIImageView! {
+        didSet {
+            lambdaImageView.isUserInteractionEnabled = true
+            
+        }
+    }
+     
+    
     private var lambdaCharacters = [UILabel]()
  
     // MARK: - View Life Cycle
@@ -24,11 +32,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setUpSubviews()
         configureNavBar()
+        setUpPanGesture()
         
-              
+        
+        
     }
     
-
+    fileprivate func setUpPanGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        panGesture.delegate = self
+        lambdaImageView.addGestureRecognizer(panGesture)
+    }
+   
+    
+   
+    @objc func handlePan(_ gestureRecognizer: UIPanGestureRecognizer)
+    
+    {
+       guard gestureRecognizer.view != nil else {return}
+       let piece = gestureRecognizer.view!
+       // Get the changes in the X and Y directions relative to
+       // the superview's coordinate space.
+       let translation = gestureRecognizer.translation(in: piece.superview)
+       if gestureRecognizer.state == .began {
+          // Save the view's original position.
+          self.initialCenter = piece.center
+       }
+          // Update the position for the .began, .changed, and .ended states
+       if gestureRecognizer.state != .cancelled {
+          // Add the X and Y translation to the view's original position.
+          let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+          piece.center = newCenter
+       }
+       else {
+          // On cancellation, return the piece to its original location.
+          piece.center = initialCenter
+       }
+        
+    }
     
     // MARK: - Methods
     
