3b21a9db227c8f2b9cc98ed1119ffab0d3f578ae
Nick Nguyen <ptnguyen1901@gmail.com>
2020-02-18 19:45:45 -0500

    Clean up code

--- Lambda Animation/Lambda Animation/ViewController.swift
+++ Lambda Animation/Lambda Animation/ViewController.swift
@@ -89,74 +89,32 @@ class ViewController: UIViewController {
         }
     }
 
+    fileprivate func createLabel(with text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = UIFont(name: "Noteworthy-Bold", size: 40)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
     
     fileprivate func setUpSubviews() {
         
-        let l : UILabel = {
-           let label = UILabel()
-            label.text = "L"
-            label.font = UIFont(name: "Noteworthy-Bold", size: 40)
-            label.textAlignment = .center
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
+        let l = createLabel(with: "L")
+        let a = createLabel(with: "a")
+        let m = createLabel(with: "m")
+        let b = createLabel(with: "b")
+        let d = createLabel(with: "d")
+        let a2 = createLabel(with: "a")
+        
         
         view.addSubview(l)
-      
-        let a : UILabel = {
-               let label = UILabel()
-                label.text = "a"
-                label.font = UIFont(name: "Noteworthy-Bold", size: 40)
-                label.textAlignment = .center
-                label.translatesAutoresizingMaskIntoConstraints = false
-                return label
-            }()
-        
         view.addSubview(a)
-       
-        let m : UILabel = {
-               let label = UILabel()
-                label.text = "m"
-                label.font = UIFont(name: "Noteworthy-Bold", size: 40)
-                label.textAlignment = .center
-                label.translatesAutoresizingMaskIntoConstraints = false
-                return label
-            }()
         view.addSubview(m)
-      
-        let b : UILabel = {
-               let label = UILabel()
-                label.text = "b"
-                label.font = UIFont(name: "Noteworthy-Bold", size: 40)
-                label.textAlignment = .center
-                label.translatesAutoresizingMaskIntoConstraints = false
-                return label
-            }()
         view.addSubview(b)
-
-        
-       
-        
-        let d : UILabel = {
-               let label = UILabel()
-                label.text = "d"
-                label.font = UIFont(name: "Noteworthy-Bold", size: 40)
-                label.textAlignment = .center
-                label.translatesAutoresizingMaskIntoConstraints = false
-                return label
-            }()
         view.addSubview(d)
-      
-        
-        let a2 : UILabel = {
-               let label = UILabel()
-                label.text = "a"
-                label.font = UIFont(name: "Noteworthy-Bold", size: 40)
-                label.textAlignment = .center
-                label.translatesAutoresizingMaskIntoConstraints = false
-                return label
-            }()
         view.addSubview(a2)
+        
         self.lambdaCharacters.append(l)
         self.lambdaCharacters.append(a)
         self.lambdaCharacters.append(m)
@@ -166,12 +124,9 @@ class ViewController: UIViewController {
      
         let lambdaStackView : UIStackView = {
             let stackView = UIStackView()
-            stackView.addArrangedSubview(l)
-            stackView.addArrangedSubview(a)
-            stackView.addArrangedSubview(m)
-            stackView.addArrangedSubview(b)
-            stackView.addArrangedSubview(d)
-            stackView.addArrangedSubview(a2)
+            for view in lambdaCharacters {
+                stackView.addArrangedSubview(view)
+            }
             stackView.alignment = .fill
             stackView.axis = .horizontal
             stackView.distribution = .fillEqually
