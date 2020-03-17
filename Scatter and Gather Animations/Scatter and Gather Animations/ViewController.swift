ecf4338afd27a1c0d717246defe41afca616ce3b
Nichole Davidson <nicholedavid@gmail.com>
2020-03-17 14:36:14 -0600

    Initial Setup

---
+++ Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Scatter and Gather Animations
+//
+//  Created by Nichole Davidson on 3/17/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
