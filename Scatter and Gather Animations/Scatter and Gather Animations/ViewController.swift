afcdd5ed872c70a6f4224ad51dd52cb746c2dcfd
Nichole Davidson <nicholedavid@gmail.com>
2020-03-17 16:14:17 -0600

    Added stackView, constraints and label text and color

--- Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
+++ Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
@@ -20,11 +20,11 @@ class ViewController: UIViewController {
     var aEndLabel = UILabel()
     var imageView = UIImageView()
     
-//    var lambdaLogo = UIImage {
-//        if UIImage.self != nil {
-//            UIImage(named: "lambda_logo")
-//        }
-//    }
+    var lambdaLogo: UIImage! {
+        
+            UIImage(named: "lambda_logo")
+        }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,15 +32,91 @@ class ViewController: UIViewController {
         setUpSubViews()
     }
     
+    func viewDidLoad(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+      
+    }
+    
     func setUpSubViews() {
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+        
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 350).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        UIImageView.animate(withDuration: 5) {
+            self.imageView.alpha = 0
+        }
+        
         view.addSubview(lLabel)
         lLabel.translatesAutoresizingMaskIntoConstraints = false
         lLabel.widthAnchor.constraint(equalTo: lLabel.heightAnchor).isActive = true
         lLabel.text = "L"
         lLabel.font = UIFont(name: "CourierNewPS-BoldMT", size: 20)
-        lLabel.backgroundColor = UIColor(displayP3Red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        lLabel.backgroundColor = .cyan
+        lLabel.textColor = .blue
         
+        view.addSubview(aLabel)
+        aLabel.translatesAutoresizingMaskIntoConstraints = false
+        aLabel.widthAnchor.constraint(equalTo: lLabel.heightAnchor).isActive = true
+        aLabel.text = "A"
+        aLabel.font = UIFont(name: "CourierNewPS-BoldMT", size: 20)
+        aLabel.backgroundColor = .blue
+        aLabel.textColor = .cyan
         
+        view.addSubview(mLabel)
+        mLabel.translatesAutoresizingMaskIntoConstraints = false
+        mLabel.widthAnchor.constraint(equalTo: lLabel.heightAnchor).isActive = true
+        mLabel.text = "M"
+        mLabel.font = UIFont(name: "CourierNewPS-BoldMT", size: 20)
+        mLabel.backgroundColor = .cyan
+        mLabel.textColor = .blue
+        
+        view.addSubview(bLabel)
+        bLabel.translatesAutoresizingMaskIntoConstraints = false
+        bLabel.widthAnchor.constraint(equalTo: lLabel.heightAnchor).isActive = true
+        bLabel.text = "B"
+        bLabel.font = UIFont(name: "CourierNewPS-BoldMT", size: 20)
+        bLabel.backgroundColor = .blue
+        bLabel.textColor = .cyan
+        
+        view.addSubview(dLabel)
+        dLabel.translatesAutoresizingMaskIntoConstraints = false
+        dLabel.widthAnchor.constraint(equalTo: lLabel.heightAnchor).isActive = true
+        dLabel.text = "D"
+        dLabel.font = UIFont(name: "CourierNewPS-BoldMT", size: 20)
+        dLabel.backgroundColor = .cyan
+        dLabel.textColor = .blue
+        
+        view.addSubview(aEndLabel)
+        aEndLabel.translatesAutoresizingMaskIntoConstraints = false
+        aEndLabel.widthAnchor.constraint(equalTo: lLabel.heightAnchor).isActive = true
+        aEndLabel.text = "A"
+        aEndLabel.font = UIFont(name: "CourierNewPS-BoldMT", size: 20)
+        aEndLabel.backgroundColor = .blue
+        aEndLabel.textColor = .cyan
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(lLabel)
+        stackView.addArrangedSubview(aLabel)
+        stackView.addArrangedSubview(mLabel)
+        stackView.addArrangedSubview(bLabel)
+        stackView.addArrangedSubview(dLabel)
+        stackView.addArrangedSubview(aEndLabel)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30)
+            
+        ])
         
         
         
@@ -51,7 +127,7 @@ class ViewController: UIViewController {
         var isScattered: Bool = true
         isScattered.toggle()
         
-        
+        imageView.center = view.center
         
     }
     
