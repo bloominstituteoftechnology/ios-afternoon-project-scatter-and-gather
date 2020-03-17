d59d4f1b61d3043888d37cd87ff53e5c6208b7d4
Mark Gerrior <github@gerrior.com>
2020-03-17 14:30:59 -0700

    Initial project and organization.

---
+++ Scatter/Scatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter
+//
+//  Created by Mark Gerrior on 3/17/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
