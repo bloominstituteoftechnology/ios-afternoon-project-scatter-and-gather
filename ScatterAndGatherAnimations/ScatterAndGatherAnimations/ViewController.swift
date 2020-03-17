ea0087df39dd2a1f664542550d01fecb09d02463
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-17 14:48:00 -0700

    Adding labels and ibOutlets

--- ScatterAndGatherAnimations/ScatterAndGatherAnimations/ViewController.swift
+++ ScatterAndGatherAnimations/ScatterAndGatherAnimations/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+ 
     enum scatOrGat: String {
         case scattered = "Scattered"
         case gathered = "Gathered"
     }
+   let someImageView: UIImageView = {
+       let theImageView = UIImageView()
+       theImageView.image = UIImage(named: "lambda_logo.png")
+       theImageView.translatesAutoresizingMaskIntoConstraints = false 
+       return theImageView
+    }()
+
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       someImageViewConstraints()
+    }
+
+    func someImageViewConstraints() {
+          view.addSubview(someImageView)
+        someImageView.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        someImageView.heightAnchor.constraint(equalToConstant: 180).isActive = true
+        someImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        someImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 28).isActive = true
+    }
+
     
     var isScattered: Bool?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+   
     @IBAction func toggleButtonPressed(_ sender: Any) {
     }
     
