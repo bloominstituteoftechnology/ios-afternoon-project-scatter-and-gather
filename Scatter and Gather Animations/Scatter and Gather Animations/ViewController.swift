4079df0f89cb77075410559e3f0dbf7a7655a576
Nichole Davidson <nicholedavid@gmail.com>
2020-03-17 15:28:45 -0600

    Added labels and imageView - adding constraints to each

--- Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
+++ Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
@@ -9,25 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var toggleButton: UIBarButtonItem!
+        
+    var lLabel = UILabel()
+    var aLabel = UILabel()
+    var mLabel = UILabel()
+    var bLabel = UILabel()
+    var dLabel = UILabel()
+    var aEndLabel = UILabel()
+    var imageView = UIImageView()
+    
+//    var lambdaLogo = UIImage {
+//        if UIImage.self != nil {
+//            UIImage(named: "lambda_logo")
+//        }
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setUpSubViews()
     }
     
-    @IBAction func toggleButtonPressed(_ sender: Any) {
+    func setUpSubViews() {
+        view.addSubview(lLabel)
+        lLabel.translatesAutoresizingMaskIntoConstraints = false
+        lLabel.widthAnchor.constraint(equalTo: lLabel.heightAnchor).isActive = true
+        lLabel.text = "L"
+        lLabel.font = UIFont(name: "CourierNewPS-BoldMT", size: 20)
+        lLabel.backgroundColor = UIColor(displayP3Red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        
+        
+        
+        
+        
+    }
+    
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var isScattered: Bool = true
+        isScattered.toggle()
+        
+        
         
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
