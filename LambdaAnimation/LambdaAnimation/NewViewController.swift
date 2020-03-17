130ce191002afe4785059c848337f30b1f746fe7
Brad <braddiroff@icloud.com>
2020-03-17 19:32:49 -0400

    Finished project

--- LambdaAnimation/LambdaAnimation/NewViewController.swift
+++ LambdaAnimation/LambdaAnimation/NewViewController.swift
@@ -12,18 +12,14 @@ class NewViewController: UIViewController {
     
     @IBOutlet var lambdaSign: UIImageView!
     
-    let randomX = Int.random(in: 1...300)
-    let randomY = Int.random(in: 1...600)
-    let randomTurn = Int.random(in: 1...4)
+    @IBOutlet var letL: UILabel!
+    @IBOutlet var letA: UILabel!
+    @IBOutlet var letM: UILabel!
+    @IBOutlet var letB: UILabel!
+    @IBOutlet var letD: UILabel!
+    @IBOutlet var letA2: UILabel!
     
     var isToggled: Bool = false
-    
-    var letterL = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-    var letterA = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-    var letterM = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-    var letterB = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-    var letterD = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-    var letterA2 = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
     
     var startPointL: CGPoint?
     var startPointA: CGPoint?
@@ -43,45 +39,72 @@ class NewViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        letterL.center = CGPoint(x: startPointL!.x, y: startPointL!.y)
-        
+
     }
     
     @IBAction func toggleButtonPressed(_ sender: Any) {
         if isToggled == false {
             messUpLetters()
         } else {
-            print("START POINT: \(startPointL)")
             returnToNormal()
         }
     }
     
     func messUpLetters() {
         
-        UIView.animateKeyframes(withDuration: 5.0, delay: 0, options: [], animations: {
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
-                self.letterL.transform = CGAffineTransform(rotationAngle: CGFloat.pi/4)
-                self.letterA.transform = CGAffineTransform(rotationAngle: CGFloat.pi/4)
+        UIView.animate(withDuration: 1) {
+            self.lambdaSign.alpha = 0
+            self.letL.textColor = self.randomColor()
+            self.letL.layer.backgroundColor = self.randombgColor()
+            self.letA.textColor = self.randomColor()
+            self.letA.layer.backgroundColor = self.randombgColor()
+            self.letM.textColor = self.randomColor()
+            self.letM.layer.backgroundColor = self.randombgColor()
+            self.letB.textColor = self.randomColor()
+            self.letB.layer.backgroundColor = self.randombgColor()
+            self.letD.textColor = self.randomColor()
+            self.letD.layer.backgroundColor = self.randombgColor()
+            self.letA2.textColor = self.randomColor()
+            self.letA2.layer.backgroundColor = self.randombgColor()
+        }
+        
+        UIView.animateKeyframes(withDuration: 3.0, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 2.5) {
+                self.letL.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat.random(in: 1...4))
+                self.letA.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat.random(in: 1...4))
+                self.letM.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat.random(in: 1...4))
+                self.letB.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat.random(in: 1...4))
+                self.letD.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat.random(in: 1...4))
+                self.letA2.transform = CGAffineTransform(rotationAngle: CGFloat.pi/CGFloat.random(in: 1...4))
+
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                self.letL.center = CGPoint(x: CGFloat.random(in: 1...300), y: CGFloat.random(in: 1...600))
+                self.letA.center = CGPoint(x: CGFloat.random(in: 1...300), y: CGFloat.random(in: 1...600))
+                self.letM.center = CGPoint(x: CGFloat.random(in: 1...300), y: CGFloat.random(in: 1...600))
+                self.letB.center = CGPoint(x: CGFloat.random(in: 1...300), y: CGFloat.random(in: 1...600))
+                self.letD.center = CGPoint(x: CGFloat.random(in: 1...300), y: CGFloat.random(in: 1...600))
+                self.letA2.center = CGPoint(x: CGFloat.random(in: 1...300), y: CGFloat.random(in: 1...600))
             }
             
-            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
-                self.letterL.transform = .identity
-                self.letterA.transform = .identity
-            }
-            
-            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25) {
-                self.letterL.center = CGPoint(x: self.view.center.x, y: self.view.center.y - 50)
-                self.letterA.center = CGPoint(x: self.view.center.x, y: self.view.center.y - 50)
-            }
-            
-            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
-                self.letterL.center = self.view.center
-                self.letterA.center = self.view.center
+            UIView.addKeyframe(withRelativeStartTime: 0.3, relativeDuration: 3) {
+             /*
+                self.letL.textColor = self.randomColor()
+                self.letL.backgroundColor = self.randomColor()
+                self.letA.textColor = self.randomColor()
+                self.letA.backgroundColor = self.randomColor()
+                self.letM.textColor = self.randomColor()
+                self.letM.backgroundColor = self.randomColor()
+                self.letB.textColor = self.randomColor()
+                self.letB.backgroundColor = self.randomColor()
+                self.letD.textColor = self.randomColor()
+                self.letD.backgroundColor = self.randomColor()
+                self.letA2.textColor = self.randomColor()
+                self.letA2.backgroundColor = self.randomColor()
+                */
             }
         }, completion: nil)
-        
-        print("START POINT: \(startPointL)")
         
         isToggled = true
         
@@ -89,114 +112,72 @@ class NewViewController: UIViewController {
     
     func returnToNormal() {
         
-        guard let spotL = startPointL, let spotA = startPointA else {
-            
-            print("It doesnt have a start point")
-            return}
+        guard let spotL = startPointL, let spotA = startPointA, let spotM = startPointM, let spotB = startPointB, let spotD = startPointD, let spotA2 = startPointA2 else {return}
         
         UIView.animate(withDuration: 2.0, animations: {
             
             self.lambdaSign.alpha = 1
-            self.letterL.transform = .identity
-            self.letterL.center = spotL
-            self.letterA.transform = .identity
-            self.letterA.center = spotA
-            self.letterM.transform = .identity
-            self.letterB.transform = .identity
-            self.letterD.transform = .identity
-            self.letterA2.transform = .identity
+            self.letL.transform = .identity
+            self.letL.center = spotL
+            self.letA.transform = .identity
+            self.letA.center = spotA
+            self.letM.transform = .identity
+            self.letM.center = spotM
+            self.letB.transform = .identity
+            self.letB.center = spotB
+            self.letD.transform = .identity
+            self.letD.center = spotD
+            self.letA2.transform = .identity
+            self.letA2.center = spotA2
+            self.letL.textColor = .black
+            self.letL.backgroundColor = .clear
+            self.letA.textColor = .black
+            self.letA.backgroundColor = .clear
+            self.letM.textColor = .black
+            self.letM.backgroundColor = .clear
+            self.letB.textColor = .black
+            self.letB.backgroundColor = .clear
+            self.letD.textColor = .black
+            self.letD.backgroundColor = .clear
+            self.letA2.textColor = .black
+            self.letA2.backgroundColor = .clear
         })
         
         isToggled = false
         
     }
     
+    func randomColor() -> UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1),
+                       alpha: 1.0)
+    }
+    
+    func randombgColor() -> CGColor {
+        return CGColor(srgbRed: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+    }
+    
     func addSubviews() {
         
         let splitSections = (self.view.frame.maxX / 6)
         let middleSection = splitSections/2
-
-        view.addSubview(letterL)
-        letterL.widthAnchor.constraint(equalTo: letterL.heightAnchor).isActive = true
-        letterL.translatesAutoresizingMaskIntoConstraints = false
-        letterL.text = "L"
-        letterL.textAlignment = .center
-        letterL.font = UIFont.systemFont(ofSize: 60)
         
-        view.addSubview(letterA)
-        letterA.widthAnchor.constraint(equalTo: letterA.heightAnchor).isActive = true
-        letterA.translatesAutoresizingMaskIntoConstraints = false
-        letterA.text = "a"
-        letterA.textAlignment = .center
-        letterA.font = UIFont.systemFont(ofSize: 60)
+        letL.center.x = middleSection
+        letA.center.x = middleSection + splitSections
+        letM.center.x = middleSection + (splitSections * 2)
+        letB.center.x = middleSection + (splitSections * 3)
+        letD.center.x = middleSection + (splitSections * 4)
+        letA2.center.x = middleSection + (splitSections * 5)
         
-        view.addSubview(letterM)
-        letterM.center = view.center
-        letterM.widthAnchor.constraint(equalTo: letterM.heightAnchor).isActive = true
-        letterM.translatesAutoresizingMaskIntoConstraints = false
-        letterM.text = "m"
-        letterM.textAlignment = .center
-        letterM.font = UIFont.systemFont(ofSize: 60)
-        
-        view.addSubview(letterB)
-        letterB.center = view.center
-        letterB.widthAnchor.constraint(equalTo: letterB.heightAnchor).isActive = true
-        letterB.translatesAutoresizingMaskIntoConstraints = false
-        letterB.text = "b"
-        letterB.textAlignment = .center
-        letterB.font = UIFont.systemFont(ofSize: 60)
-        
-        view.addSubview(letterD)
-        letterD.center = view.center
-        letterD.widthAnchor.constraint(equalTo: letterD.heightAnchor).isActive = true
-        letterD.translatesAutoresizingMaskIntoConstraints = false
-        letterD.text = "d"
-        letterD.textAlignment = .center
-        letterD.font = UIFont.systemFont(ofSize: 60)
-     
-        view.addSubview(letterA2)
-        letterA2.center = view.center
-        letterA2.widthAnchor.constraint(equalTo: letterA2.heightAnchor).isActive = true
-        letterA2.translatesAutoresizingMaskIntoConstraints = false
-        letterA2.text = "a"
-        letterA2.textAlignment = .center
-        letterA2.font = UIFont.systemFont(ofSize: 60)
-    /*
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackView)
-        stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
-        
-        stackView.addArrangedSubview(letterL)
-        stackView.addArrangedSubview(letterA)
-        stackView.addArrangedSubview(letterM)
-        stackView.addArrangedSubview(letterB)
-        stackView.addArrangedSubview(letterD)
-        stackView.addArrangedSubview(letterA2)
-      
-        print("START POINT: \(startPointL)")
-      
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
-        ])
- */
-    
-        startPointL = CGPoint(x: middleSection, y: 30)
-        startPointA = CGPoint(x: middleSection + splitSections , y: 30)
+        startPointL = CGPoint(x: letL.center.x, y: letL.center.y)
+        startPointA = CGPoint(x: letA.center.x , y: letA.center.y)
+        startPointM = CGPoint(x: letM.center.x, y: letM.center.y)
+        startPointB = CGPoint(x: letB.center.x, y: letB.center.y)
+        startPointD = CGPoint(x: letD.center.x, y: letD.center.y)
+        startPointA2 = CGPoint(x: letA2.center.x, y: letA2.center.y)
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
