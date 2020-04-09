aeb44ea4b9323c85a16c9603e43bca19153d2f97
Matt Martindale <martindalematt18@yahoo.com>
2020-04-08 20:44:07 -0600

    Initial commit

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Matthew Martindale on 4/8/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
