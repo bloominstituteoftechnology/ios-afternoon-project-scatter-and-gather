91132a4d2665ce61ecbe1ff2eda7a5ff7f36da29
Matt Martindale <martindalematt18@yahoo.com>
2020-04-09 22:39:33 -0600

    set up outlets and animated logo

--- Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -11,15 +11,31 @@ import UIKit
 class ViewController: UIViewController {
     
     var isScattered: Bool = false
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var secondALabel: UILabel!
+    @IBOutlet weak var lambdaLogoImageView: UIImageView!
+    
     @IBAction func toggleButtonTapped(_ sender: UIBarButtonItem) {
         isScattered.toggle()
+        
+        if isScattered == true {
+            
+            UIView.animate(withDuration: 2) {
+                self.lambdaLogoImageView.alpha = 0
+            }
+            
+        } else {
+            UIView.animate(withDuration: 2) {
+                self.lambdaLogoImageView.alpha = 1
+            }
+        }
     }
-    
 }
+
+
 
