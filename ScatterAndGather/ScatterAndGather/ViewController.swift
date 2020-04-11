4426fe431c1afed414b86e444a1305ede1001a02
Libby Thomas <thomaslibby05@gmail.com>
2020-04-10 20:03:48 -0500

    Initial Commit

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Elizabeth Thomas on 4/10/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
