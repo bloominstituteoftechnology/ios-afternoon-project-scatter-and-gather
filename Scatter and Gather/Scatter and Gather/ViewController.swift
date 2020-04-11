18fa24d3709afc34d1023d86a65b71ba3da3f62a
Jarren Campos <jarrenmcampos@gmail.com>
2020-04-10 20:44:46 -0700

    Adding Rotation Functions working around bugs

--- Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController {
         return randomInt
     }
     
-    func randomBackgroundColors() ->  UIColor{
+    func randomColors() ->  UIColor{
         let colorArray: [UIColor] = [.blue, .red, .green, .cyan, .gray, .orange]
         let randomInt = Int.random(in: 0...(colorArray.count - 1))
         let pickedColor: UIColor = colorArray[randomInt]
@@ -62,31 +62,43 @@ class ViewController: UIViewController {
             
             UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
                 self.lLabel.transform = CGAffineTransform(translationX: CGFloat(self.randomXGenerator()), y: CGFloat(self.randomYGenerator()))
-                self.lLabel.backgroundColor = self.randomBackgroundColors()
+                self.lLabel.transform = CGAffineTransform(rotationAngle: .pi/4)
+                self.lLabel.backgroundColor = self.randomColors()
+                self.lLabel.textColor = self.randomColors()
             }, completion: nil)
             
             UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
-                self.aLabel.backgroundColor = self.randomBackgroundColors()
+                self.aLabel.backgroundColor = self.randomColors()
+                self.aLabel.textColor = self.randomColors()
+                
                 self.aLabel.transform = CGAffineTransform(translationX: CGFloat(self.randomXGenerator()), y: CGFloat(self.randomYGenerator()))
             }, completion: nil)
             
             UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
-                self.mLabel.backgroundColor = self.randomBackgroundColors()
+                self.mLabel.backgroundColor = self.randomColors()
+                self.mLabel.textColor = self.randomColors()
+                
                 self.mLabel.transform = CGAffineTransform(translationX: CGFloat(self.randomXGenerator()), y: CGFloat(self.randomYGenerator()))
             }, completion: nil)
             
             UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
-                self.bLabel.backgroundColor = self.randomBackgroundColors()
+                self.bLabel.backgroundColor = self.randomColors()
+                self.bLabel.textColor = self.randomColors()
+                
                 self.bLabel.transform = CGAffineTransform(translationX: CGFloat(self.randomXGenerator()), y: CGFloat(self.randomYGenerator()))
             }, completion: nil)
             
             UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
-                self.dLabel.backgroundColor = self.randomBackgroundColors()
+                self.dLabel.backgroundColor = self.randomColors()
+                self.dLabel.textColor = self.randomColors()
+                
                 self.dLabel.transform = CGAffineTransform(translationX: CGFloat(self.randomXGenerator()), y: CGFloat(self.randomYGenerator()))
             }, completion: nil)
             
             UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
-                self.aALabel.backgroundColor = self.randomBackgroundColors()
+                self.aALabel.backgroundColor = self.randomColors()
+                self.aALabel.textColor = self.randomColors()
+                
                 self.aALabel.transform = CGAffineTransform(translationX: CGFloat(self.randomXGenerator()), y: CGFloat(self.randomYGenerator()))
             }, completion: nil)
             
@@ -96,16 +108,22 @@ class ViewController: UIViewController {
             UIView.animate(withDuration: 1) {
                 self.lLabel.transform = .identity
                 self.lLabel.backgroundColor = .clear
+                self.lLabel.textColor = .black
                 self.aLabel.transform = .identity
                 self.aLabel.backgroundColor = .clear
+                self.aLabel.textColor = .black
                 self.mLabel.transform = .identity
                 self.mLabel.backgroundColor = .clear
+                self.mLabel.textColor = .black
                 self.bLabel.transform = .identity
                 self.bLabel.backgroundColor = .clear
+                self.bLabel.textColor = .black
                 self.dLabel.transform = .identity
                 self.dLabel.backgroundColor = .clear
+                self.dLabel.textColor = .black
                 self.aALabel.transform = .identity
                 self.aALabel.backgroundColor = .clear
+                self.aALabel.textColor = .black
                 self.lambdaLogo.alpha = 1.0
             }
             isScattered = false
