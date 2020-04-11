a29350523335fa9970eea03fdcbf4785bc5c6994
morgan smith <mssmitty07@gmail.com>
2020-04-11 00:31:44 -0400

    final commit

--- Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -24,55 +24,95 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var logo: UIImageView!
     
-    var isScattered: Bool = false
+    var isScattered: Bool = true
     
     override func viewDidLoad() {
-          super.viewDidLoad()
+        super.viewDidLoad()
         //  let letters = [one, two, three, four, five, six]
-          
-      }
+        
+    }
     
     @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
         if isScattered == true {
-        
-        UIView.animateKeyframes(withDuration: 3, delay: 0, options: [], animations: {
-           UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.one.textColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.two.textColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.three.textColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.four.textColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.five.textColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.six.textColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.one.backgroundColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.two.backgroundColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.three.backgroundColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.four.backgroundColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.five.backgroundColor = .random()
-            }
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                self.six.backgroundColor = .random()
-            }
-        }, completion: nil)
+            
+            UIView.animateKeyframes(withDuration: 3, delay: 0, options: [], animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.one.textColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.one.transform = CGAffineTransform(rotationAngle: .random())
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.one.transform = CGAffineTransform(translationX: .random() * 200 , y: .random() * 400)
+                               }
+                
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.two.textColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.two.transform = CGAffineTransform(rotationAngle: .random())
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                self.two.transform = CGAffineTransform(translationX: .random() * 200 , y: .random() * 400)
+                           }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.three.textColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.three.transform = CGAffineTransform(rotationAngle: .random())
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                self.three.transform = CGAffineTransform(translationX: .random() * 200 , y: .random() * 400)
+                           }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.four.textColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.four.transform = CGAffineTransform(rotationAngle: .random())
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                self.four.transform = CGAffineTransform(translationX: .random() * 200 , y: .random() * 400)
+                           }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.five.textColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.five.transform = CGAffineTransform(rotationAngle: .random())
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                self.five.transform = CGAffineTransform(translationX: .random() * 200 , y: .random() * 400)
+                           }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.six.textColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.six.transform = CGAffineTransform(rotationAngle: .random())
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                self.six.transform = CGAffineTransform(translationX: .random() * 200 , y: .random() * 400)
+                           }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.one.backgroundColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.two.backgroundColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.three.backgroundColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.four.backgroundColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.five.backgroundColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.six.backgroundColor = .random()
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.logo.alpha = 0
+                }
+            }, completion: nil)
             isScattered = false
         } else if isScattered == false {
             UIView.animateKeyframes(withDuration: 3.0, delay: 0, options: [], animations: {
@@ -80,21 +120,60 @@ class ViewController: UIViewController {
                     self.one.transform = .identity
                 }
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                                  self.two.transform = .identity
-                              }
+                    self.one.textColor = .black
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.one.backgroundColor = .white
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.two.transform = .identity
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.two.textColor = .black
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.two.backgroundColor = .white
+                }
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
                     self.three.transform = .identity
                 }
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                                  self.four.transform = .identity
-                              }
+                    self.three.textColor = .black
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.three.backgroundColor = .white
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.four.transform = .identity
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.four.backgroundColor = .white
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.four.textColor = .black
+                }
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
                     self.five.transform = .identity
                 }
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
-                                   self.six.transform = .identity
+                    self.five.textColor = .black
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.five.backgroundColor = .white
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.six.transform = .identity
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.six.textColor = .black
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                    self.six.backgroundColor = .white
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 3) {
+                                   self.logo.alpha = 1
                                }
-              
+                
             }, completion: nil)
             isScattered = true
         }
@@ -114,10 +193,10 @@ extension CGFloat {
 extension UIColor {
     static func random() -> UIColor {
         return UIColor(
-           red:   .random(),
-           green: .random(),
-           blue:  .random(),
-           alpha: 1.0
+            red:   .random(),
+            green: .random(),
+            blue:  .random(),
+            alpha: 1.0
         )
     }
 }
