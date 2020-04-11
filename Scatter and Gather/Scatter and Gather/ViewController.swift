0ec7aa74e47ba361cd37b4bd66c692888d5ac6ec
morgan smith <mssmitty07@gmail.com>
2020-04-10 21:09:39 -0400

    initial commit

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Morgan Smith on 4/10/20.
+//  Copyright © 2020 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    var isScattered: Bool
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        isScattered = false
+    }
+    
+
+
+}
+
