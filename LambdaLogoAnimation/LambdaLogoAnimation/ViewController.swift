f5d0a9c14b03fbfe1d73a32840cfe38c0f8dafca
kensam1 <samarakodykennedy@gmail.com>
2020-04-10 23:37:35 -0400

    Animation Project

---
+++ LambdaLogoAnimation/LambdaLogoAnimation/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  LambdaLogoAnimation
+//
+//  Created by Lambda_School_Loaner_241 on 4/10/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var label1: UILabel!
+    var label2: UILabel!
+    var label3: UILabel!
+    var label4: UILabel!
+    var label5: UILabel!
+    var label6: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func toggleButtonTapped(_ sender: Any) {
+        
+        let isScattered: Bool = false 
+        
+    }
+    
+    private func configureLabel(){
+        
+    }
+    
+
+}
+
