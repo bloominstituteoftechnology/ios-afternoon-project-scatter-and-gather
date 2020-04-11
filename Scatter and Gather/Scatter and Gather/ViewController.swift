5bdfb677ab26d270ada228c9100575e37b0959e5
Kevin Stewart <kistewart419@gmail.com>
2020-04-10 23:43:57 -0400

    MVP

--- Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -10,293 +10,137 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var image: Image?
-    private var stackView: UIStackView?
-    
+    private var isScattered = false
+    private var imageView = UIImageView()
+    private var stackView = UIStackView()
+    private var toggleButton = UIBarButtonItem()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         configureStackView()
         configureLabels()
-        updateViews()
     }
-    
-        override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            
-        }
 
     // MARK: - Programmatic Constraints
-    
-    private func updateViews() {
-       
-    }
-    
 
     private func configureStackView() {
-        let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackView)
 
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
+        stackView.spacing = 5
+        stackView.center = view.center
 
         NSLayoutConstraint.activate([
-        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
         ])
     }
     
     private func configureLabels() {
-        
-        
+
         // Labels
         let lLabel = UILabel()
             lLabel.translatesAutoresizingMaskIntoConstraints = false
-            lLabel.textAlignment = .center
             lLabel.text = "L"
             lLabel.font = UIFont.systemFont(ofSize: 50)
-        stackView?.addSubview(lLabel)
+            stackView.addArrangedSubview(lLabel)
             
-        
         let a1Label = UILabel()
             a1Label.translatesAutoresizingMaskIntoConstraints = false
-            a1Label.textAlignment = .center
             a1Label.text = "A"
             a1Label.font = UIFont.systemFont(ofSize: 50)
-            stackView?.addSubview(a1Label)
+            stackView.addArrangedSubview(a1Label)
             
-        
         let mLabel = UILabel()
             mLabel.translatesAutoresizingMaskIntoConstraints = false
-            mLabel.textAlignment = .center
             mLabel.text = "M"
             mLabel.font = UIFont.systemFont(ofSize: 50)
-            stackView?.addSubview(mLabel)
+            stackView.addArrangedSubview(mLabel)
             
-        
         let bLabel = UILabel()
             bLabel.translatesAutoresizingMaskIntoConstraints = false
-            bLabel.textAlignment = .center
             bLabel.text = "B"
             bLabel.font = UIFont.systemFont(ofSize: 50)
-            stackView?.addSubview(bLabel)
+            stackView.addArrangedSubview(bLabel)
             
-        
         let dLabel = UILabel()
             dLabel.translatesAutoresizingMaskIntoConstraints = false
-            dLabel.textAlignment = .center
             dLabel.text = "D"
             dLabel.font = UIFont.systemFont(ofSize: 50)
-            stackView?.addSubview(dLabel)
+            stackView.addArrangedSubview(dLabel)
             
-        
         let a2Label = UILabel()
             a2Label.translatesAutoresizingMaskIntoConstraints = false
-            a2Label.textAlignment = .center
             a2Label.text = "A"
             a2Label.font = UIFont.systemFont(ofSize: 50)
-            stackView?.addSubview(a2Label)
+            stackView.addArrangedSubview(a2Label)
             
-        
-        let imageView = UIImageView()
+        // Image logo
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .center
-        imageView.image = image?.image
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "lambda_logo")
         view.addSubview(imageView)
         
-        
-        
-//        // MARK: - L label constraints
-//
-//        NSLayoutConstraint(item: lLabel,
-//                           attribute: .top,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .top,
-//                           multiplier: 1,
-//                           constant: 20).isActive = true
-//        NSLayoutConstraint(item: lLabel,
-//                           attribute: .leading,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .leading,
-//                           multiplier: 1,
-//                           constant: 10).isActive = true
-//        NSLayoutConstraint(item: lLabel,
-//                           attribute: .trailing,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .trailing,
-//                           multiplier: 1,
-//                           constant: -10).isActive = true
-//
-//
-//
-//        // MARK: - A1 label constraints
-//        NSLayoutConstraint(item: a1Label,
-//                           attribute: .top,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .top,
-//                           multiplier: 1,
-//                           constant: 20).isActive = true
-//        NSLayoutConstraint(item: a1Label,
-//                           attribute: .leading,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .leading,
-//                           multiplier: 1,
-//                           constant: 25).isActive = true
-//        NSLayoutConstraint(item: a1Label,
-//                           attribute: .trailing,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .trailing,
-//                           multiplier: 1,
-//                           constant: -25).isActive = true
-//
-//        // MARK: - M label constraints
-//        NSLayoutConstraint(item: mLabel,
-//                           attribute: .top,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .top,
-//                           multiplier: 1,
-//                           constant: 20).isActive = true
-//        NSLayoutConstraint(item: mLabel,
-//                           attribute: .leading,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .leading,
-//                           multiplier: 1,
-//                           constant: 50).isActive = true
-//        NSLayoutConstraint(item: mLabel,
-//                           attribute: .trailing,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .trailing,
-//                           multiplier: 1,
-//                           constant: -50).isActive = true
-//        // MARK: - B label constraints
-//        NSLayoutConstraint(item: bLabel,
-//                           attribute: .top,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .top,
-//                           multiplier: 1,
-//                           constant: 20).isActive = true
-//        NSLayoutConstraint(item: bLabel,
-//                           attribute: .leading,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .leading,
-//                           multiplier: 1,
-//                           constant: 70).isActive = true
-//        NSLayoutConstraint(item: bLabel,
-//                           attribute: .trailing,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .trailing,
-//                           multiplier: 1,
-//                           constant: -70).isActive = true
-//        // MARK: - D label constraints
-//        NSLayoutConstraint(item: dLabel,
-//                           attribute: .top,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .top,
-//                           multiplier: 1,
-//                           constant: 20).isActive = true
-//        NSLayoutConstraint(item: dLabel,
-//                           attribute: .leading,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .leading,
-//                           multiplier: 1,
-//                           constant: 100).isActive = true
-//        NSLayoutConstraint(item: dLabel,
-//                           attribute: .trailing,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .trailing,
-//                           multiplier: 1,
-//                           constant: -100).isActive = true
-//        // MARK: - A2 label constraints
-//        NSLayoutConstraint(item: a2Label,
-//                           attribute: .top,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .top,
-//                           multiplier: 1,
-//                           constant: 20).isActive = true
-//        NSLayoutConstraint(item: a2Label,
-//                           attribute: .leading,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .leading,
-//                           multiplier: 1,
-//                           constant: 130).isActive = true
-//        NSLayoutConstraint(item: a2Label,
-//                           attribute: .trailing,
-//                           relatedBy: .equal,
-//                           toItem: view.safeAreaLayoutGuide,
-//                           attribute: .trailing,
-//                           multiplier: 1,
-//                           constant: -130).isActive = true
-        
-        // MARK: - Image View
-     let imageViewTopConstraint = NSLayoutConstraint(item: imageView,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: view.safeAreaLayoutGuide,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: 100).isActive = true
-       let imageViewLeadingConstriant = NSLayoutConstraint(item: imageView,
-                           attribute: .leading,
-                           relatedBy: .equal,
-                           toItem: view.safeAreaLayoutGuide,
-                           attribute: .leading,
-                           multiplier: 1,
-                           constant: 25).isActive = true
-        let imageViewTrailingConstraint = NSLayoutConstraint(item: imageView,
-                           attribute: .trailing,
-                           relatedBy: .equal,
-                           toItem: view.safeAreaLayoutGuide,
-                           attribute: .trailing,
-                           multiplier: 1,
-                           constant: -25).isActive = true
-
-
-
+        NSLayoutConstraint.activate([imageView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 25),
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+        imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
     }
 
+    // MARK: - Methods
+    
+    // Scatter function
+    @objc private func scatter() {
+        if isScattered {
+            isScattered = false
+            UIView.animate(withDuration: 2.0) {
+                for view in self.stackView.arrangedSubviews {
+                    if let label = view as? UILabel {
+                        label.alpha = 0
+                        self.stackView.addArrangedSubview(label) //brings everything back into place
+                        label.alpha = 1
+                        self.imageView.alpha = 1
+                        label.textColor = .black
+                        label.backgroundColor = .clear
+                        label.transform = .identity //label turns upright
+                    }
+                }
+            }
+        } else {
+            isScattered = true
+            UIView.animate(withDuration: 2.0) {
+                for view in self.stackView.arrangedSubviews {
+                    if let label = view as? UILabel {
+                        self.randomColor(label)
+                        let maxX = self.view.bounds.maxX - label.bounds.width - label.bounds.maxX
+                        let randomX = CGFloat.random(in: 0..<maxX)
+                        
+                        let maxY = self.view.bounds.maxY - label.bounds.maxY - label.bounds.height - 50 // -50 because stack view is 50 points from the top of the view
+                        let randomY = CGFloat.random(in: 0..<maxY)
+                        
+                        label.layer.position = CGPoint(x: randomX, y: randomY)
+                        label.transform = CGAffineTransform(rotationAngle: CGFloat.pi / CGFloat.random(in: 0...8))
+                        self.imageView.alpha = 0
+                    }
+                }
+            }
+        }
+    }
 
-
-
-
-
-
-
+    // Picks a random color
+    private func randomColor(_ label: UILabel) {
+        let diffTextColor = CGFloat.random(in: 0.1...3.0) //picks a random color between these two hue's
+        label.textColor = UIColor(hue: diffTextColor, saturation: diffTextColor, brightness: diffTextColor, alpha: diffTextColor)
+        let diffBackroundColor = CGFloat.random(in: 0.1...3.0)
+        label.backgroundColor = UIColor(hue: diffBackroundColor, saturation: diffBackroundColor, brightness: diffBackroundColor, alpha: diffBackroundColor)
+    }
 
     // MARK: - Actions
     @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
-        
-        var isScattered: Bool
-        
+        scatter()
+        }
     }
-
-
-
-
-
-
-
-
-
-}
-
 
