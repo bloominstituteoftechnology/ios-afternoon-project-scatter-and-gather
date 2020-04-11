6bdc2339d623d989874372b4936bf5726b6df315
Chad Parker <chad@chadparker.com>
2020-04-10 20:30:15 -0700

    Add Xcode project, set up view controller in storyboard

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Chad Parker on 4/10/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var isScattered = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+        
+    }
+}
+
