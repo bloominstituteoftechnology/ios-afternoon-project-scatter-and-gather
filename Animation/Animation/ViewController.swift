22918e7b1ee9690ff50d0f7e2b811e8e1aeaf785
Claudia Maciel <clc_80@yahoo.com>
2020-04-10 17:33:13 -0700

    UI is set up

--- Animation/Animation/ViewController.swift
+++ Animation/Animation/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Properties
+    var isScattered: Bool = false
+    
+    //MARK: - IBOutlets
+    @IBOutlet var lLabel: UILabel!
+    @IBOutlet var aLabel: UILabel!
+    @IBOutlet var mLabel: UILabel!
+    @IBOutlet var bLabel: UILabel!
+    @IBOutlet var dLabel: UILabel!
+    @IBOutlet var a2Label: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - IBAction
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
