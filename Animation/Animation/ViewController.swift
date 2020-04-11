989aef1a10fa9e13920df2934c18fb68f3553a2c
Claudia Maciel <clc_80@yahoo.com>
2020-04-10 17:16:03 -0700

    Initial Commit

---
+++ Animation/Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Claudia Contreras on 4/10/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
