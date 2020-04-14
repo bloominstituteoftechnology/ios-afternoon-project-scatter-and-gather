c940b4ae5287a6956337aae4bc0ef7b9af01ed24
Shawn <shawn.james@me.com>
2020-04-14 17:51:42 -0400

    setup animation sequence skeleton

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lambdaLogoImageView: UIImageView!
+    @IBOutlet weak var letterLabels: UILabel!
+    
+    
     private var isScattered = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func toggleButtonPressed(_ sender: Any) {
     }
     
-
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+        
+        switch isScattered {
+        case true:
+            // perform gather animation sequence
+            resetLetters()
+            DispatchQueue.main.asyncAfter(deadline: .now() + <#what time do I want for this?#>) {
+                self.fadeInLogo(self.lambdaLogoImageView)
+            }
+        default:
+            // perform scatter animation sequence
+            fadeOutLogo(lambdaLogoImageView)
+            DispatchQueue.main.asyncAfter(deadline: .now() + <#what time do I want for this?#>) {
+                moveLettersToRandomLocations()
+                assignLettersRandomColors()
+                rotateLetters()
+            }
+        }
+        
+        // toggle isScattered
+        
+        isScattered.toggle()
+    }
+    
+    
+    // MARK: - Scatter Animation Methods
+    
+    private func fadeOutLogo(_ logo: UIImageView) {
+        UIView.animate(withDuration: 4) { logo.alpha = 0 }
+    }
+    
+    private func moveLettersToRandomLocations() {
+        
+    }
+    
+    private func assignLettersRandomColors() {
+        
+    }
+    
+    private func rotateLetters() {
+        
+    }
+    
+    // MARK: - Gather Animation Methods
+    
+    private func fadeInLogo(_ logo: UIImageView) {
+        
+    }
+    
+    private func resetLetters() {
+        // .identity <-resets to original state
+    }
+    
 }
 
