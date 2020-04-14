1da623c83dfab60aee164035b64252fcc03b9bb5
hai666l <0xconr@protonmail.com>
2020-04-14 09:46:26 -0500

    Create Views

--- ScatterAndGather/ScatterAndGather/View Controllers/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/View Controllers/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var L: UILabel!
+    @IBOutlet weak var A: UILabel!
+    @IBOutlet weak var M: UILabel!
+    @IBOutlet weak var B: UILabel!
+    @IBOutlet weak var D: UILabel!
+    @IBOutlet weak var A2: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
     var isScattered: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
