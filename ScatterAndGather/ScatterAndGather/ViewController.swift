fa870949fd5ad388c5db2d6c373c19b8e33d4c8e
Shawn <shawn.james@me.com>
2020-04-14 10:51:31 -0400

    add isScattered var to hold scattered/gathered state

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var isScattered = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
