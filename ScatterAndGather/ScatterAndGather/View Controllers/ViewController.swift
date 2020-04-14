9799c544da17a1292f23882f18161b3c9f3dd88e
hai666l <0xconr@protonmail.com>
2020-04-14 09:05:16 -0500

    Add a bar button to the navigation bar

---
+++ ScatterAndGather/ScatterAndGather/View Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by conner on 4/14/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var isScattered: Bool = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+    }
+}
