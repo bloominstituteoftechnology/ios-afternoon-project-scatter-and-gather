e7213e6123197b108e5d0c3fa0fed38b69e7fa33
Harmony Radley <harmonyradley99@gmail.com>
2020-04-14 22:37:04 -0400

    Created Xcode project and followed steps for 'Add a Bar Button to the Navigation Bar'

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Harmony Radley on 4/14/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var isScattered: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+}
+
