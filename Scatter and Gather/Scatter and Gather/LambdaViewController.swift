01934306acad3f2d311a078e6bb5b9c3f534268a
Harmony Radley <harmonyradley99@gmail.com>
2020-04-15 18:41:38 -0400

    Almost done with scatter animation. Will be working on gather animation

--- Scatter and Gather/Scatter and Gather/LambdaViewController.swift
+++ Scatter and Gather/Scatter and Gather/LambdaViewController.swift
@@ -49,15 +49,8 @@ class LambdaViewController: UIViewController {
         // addSubview(imageView)
     }
     
-    // off set ?
-    // random. Don't know how to make them random.
-    // Trying to access transform to make random postion.
-    // confused on how to do this even after reading the Int API
-    // cast the Int? as? Int.self as? CGFloat
-    
-    // If I set constraints in storyboard... random locations?
     private func lettersRandom() {
-  
+        
         UIView.animateKeyframes(withDuration: 4.0, delay: 0, options: [], animations: {
             
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
@@ -70,28 +63,29 @@ class LambdaViewController: UIViewController {
             
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
                 self.labelM.transform = CGAffineTransform(translationX: 50, y: 100)
+                self.labelM.transform = CGAffineTransform(rotationAngle: .pi/0.8)
+                self.labelM.backgroundColor = UIColor(red: .random(in: 0...255), green: .random(in: 0...255), blue: .random(in: 0...255), alpha: 1.0)
+                self.labelM.textColor = UIColor(red: .random(in: 0...255), green: .random(in: 0...255), blue: .random(in: 0...255), alpha: 1.0)
             }
             
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
                 self.labelD.transform = CGAffineTransform(translationX: 45, y: 60)
+                self.labelD.backgroundColor = UIColor(red: .random(in: 0...255), green: .random(in: 0...255), blue: .random(in: 0...255), alpha: 1.0)
+                self.labelD.textColor = UIColor(red: .random(in: 0...255), green: .random(in: 0...255), blue: .random(in: 0...255), alpha: 1.0)
+                self.labelD.transform = CGAffineTransform(rotationAngle: .pi/16.0)
             }
             
             
-            
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
+                self.labelA2.transform = CGAffineTransform(translationX: 15, y: 70)
+                self.labelA2.backgroundColor = UIColor(red: .random(in: 0...255), green: .random(in: 0...255), blue: .random(in: 0...255), alpha: 1.0)
+                self.labelA2.textColor = UIColor(red: .random(in: 0...255), green: .random(in: 0...255), blue: .random(in: 0...255), alpha: 1.0)
+                self.labelA2.transform = CGAffineTransform(rotationAngle: .pi/4)
+            }
             
             
         }, completion: nil)
-        // array of labels
-        //
-}
-
-}
-//rgb 0 - 255
-extension UIColor {
-    static var random: UIColor {
-        return UIColor(red: .random(in: 0...1),
-                       green: .random(in: 0...1),
-                       blue: .random(in: 0...1),
-                       alpha: 1.0)
+        
     }
+    
 }
