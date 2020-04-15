f39145b8a579ab30b0a905cb87df229c881b676a
Harmony Radley <harmonyradley99@gmail.com>
2020-04-15 17:29:30 -0400

    latest commit

--- Scatter and Gather/Scatter and Gather/LambdaViewController.swift
+++ Scatter and Gather/Scatter and Gather/LambdaViewController.swift
@@ -28,10 +28,47 @@ class LambdaViewController: UIViewController {
     }
 
     @IBAction func toggleButtonPressed(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+        
     }
     
+    // I don't know where to find the information on fading something..
+    // alpha 1.0 - 0.0 ( fade out )
+    private func fadeLogo() {
+        UIView.animate(withDuration: 4.0, delay: 0.0, options: UIView.AnimationOptions.curveLinear, animations: {
+            self.imageView.alpha = 0.0
+        }, completion: nil)
+        
+        // addSubview(imageView)
+    }
     
+    // off set ?
+    // random. Don't know how to make them random.
+    // Trying to access transform to make random postion.
+    // confused on how to do this even after reading the Int API
+    // cast the Int? as? Int.self as? CGFloat
     
-    
+    // If I set constraints in storyboard... random locations?
+    private func lettersRandom() {
+        // label.center = CGPoint(x: view.center.x, y: -label.bounds.size.height)
+        
+        CGFloat.random(in: 0...20)
+        // array of labels
+        //
 }
 
+}
+//rgb 0 - 255
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1),
+                       alpha: 1.0)
+    }
+}
