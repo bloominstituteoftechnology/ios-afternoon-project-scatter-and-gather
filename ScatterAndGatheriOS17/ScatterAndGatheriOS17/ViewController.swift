75f979517b20c9a1f7d0a10377b0f7822ddb2762
Stephanie Ballard <user.name>
2020-05-12 11:16:40 -0700

    initial commit

---
+++ ScatterAndGatheriOS17/ScatterAndGatheriOS17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterAndGatheriOS17
+//
+//  Created by Stephanie Ballard on 5/12/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
