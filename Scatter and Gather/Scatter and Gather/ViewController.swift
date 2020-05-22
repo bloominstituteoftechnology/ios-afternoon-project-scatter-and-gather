9fce64647bc8aaf591a0c15a0ae5ec56f907c6f4
Robert Vance <Fearmydotzs@gmail.com>
2020-05-21 21:42:53 -0600

    Initial Commit

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Rob Vance on 5/21/20.
+//  Copyright © 2020 Robs Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
