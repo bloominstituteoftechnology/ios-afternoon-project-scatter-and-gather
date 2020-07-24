f1ca51fd1f1f91d58e4a9f1cac672041803914ad
BrysonSaclausa <brysonSaclausa@Owners-MacBook-Pro.local>
2020-07-24 16:32:23 -0700

    got one letter to animate

--- iosScatter&GatherProject/iosScatter&GatherProject/ViewController.swift
+++ iosScatter&GatherProject/iosScatter&GatherProject/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isScattered: Bool = false
+    
+    @IBOutlet weak var labelL: UILabel!
+    @IBOutlet weak var labelA: UILabel!
+    @IBOutlet weak var labelM: UILabel!
+    @IBOutlet weak var labelB: UILabel!
+    @IBOutlet weak var labelD: UILabel!
+    @IBOutlet weak var labelA2: UILabel!
+    @IBOutlet weak var lambdaLogoImage: UIImageView!
+    
+ 
+    
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+        print("togglePressed")
+        labelA.center = view.center
+        UIView.animateKeyframes(withDuration: 3.0, delay: 0, options: [], animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5) {
+                self.labelA.transform = CGAffineTransform(translationX: 0, y: -50)
+            }
+                UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
+                self.labelA.transform = .identity
+            }
+        }, completion: nil)
+      
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
