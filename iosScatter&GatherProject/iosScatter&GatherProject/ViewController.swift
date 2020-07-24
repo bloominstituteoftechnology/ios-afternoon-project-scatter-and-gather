ac7c24da1c1516188ee6cc3ae7d5f00a9aa60530
BrysonSaclausa <brysonSaclausa@Owners-MacBook-Pro.local>
2020-07-23 23:33:54 -0700

    createdProjectFile

---
+++ iosScatter&GatherProject/iosScatter&GatherProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosScatter&GatherProject
+//
+//  Created by BrysonSaclausa on 7/23/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
