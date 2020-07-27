6079bab819ea4495827487df05c382e5418f62af
Ben James <bejjmail@gmail.com>
2020-07-26 20:38:11 -0700

    created project, added bar button

---
+++ scatterAndGather/scatterAndGather/ScatterAndGatherViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ScatterAndGatherViewController.swift
+//  scatterAndGather
+//
+//  Created by Dojo on 7/26/20.
+//  Copyright © 2020 Dojo. All rights reserved.
+//
+
+import UIKit
+
+class ScatterAndGatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func toggleButtonPressed(_: Any) {
+        var isScattered: Bool
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
